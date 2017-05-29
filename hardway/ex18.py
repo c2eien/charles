940ce54a91d5f2b89e58398eca6e0616f4c5a07a
c2eien <tocharleschang@gmail.com>
2017-05-28 22:53:24 -0700

    -v

--- hardway/ex18.py
+++ hardway/ex18.py
@@ -2,6 +2,11 @@
 def print_two(*args):
 	arg1, arg2 = args
 	print("arg1: {}, arg2: {}".format(arg1, arg2))
+#this is not a good way. 
+# The problem with print_two is that it's not the easiest way to make a function. 
+# In Python we can skip the whole unpacking arguments 
+# and just use the names we want right inside (). 
+
 
 # ok, that *args is actually pointless, we can just do this
 def print_two_again(arg1, arg2):
